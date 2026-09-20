85993c1cb0e64cc55e992fa0e524f84abc3c64d1
ahmedhimel21 <ahmedhimel000@gmail.com>
2026-09-20 21:35:52 +0600

    refactor

--- src/components/MoviesNavbar.jsx
+++ src/components/MoviesNavbar.jsx
@@ -1,4 +1,4 @@
-import { Clapperboard, Home, Search } from "lucide-react";
+import { Clapperboard, Search } from "lucide-react";
 import { NavLink } from "react-router";
 
 export default function MoviesNavbar({ search, setSearch }) {
@@ -16,16 +16,6 @@ export default function MoviesNavbar({ search, setSearch }) {
             <span>
               Movie<span className="text-[#ff3340]">Box</span>
             </span>
-          </NavLink>
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              `group flex items-center gap-2 rounded-xl px-4 py-2.5 text-sm font-bold transition-all duration-300 ${isActive ? "bg-primary text-primary-content shadow-lg shadow-primary/20" : "text-slate-400 hover:bg-white/5 hover:text-white"}`
-            }
-          >
-            {" "}
-            <Home className="size-4 transition-transform group-hover:scale-110" />{" "}
-            <span>Home</span>{" "}
           </NavLink>
           <label className="flex w-full max-w-xl items-center gap-3 rounded-xl border border-white/10 bg-white/5 px-4 transition focus-within:border-red-500/70 focus-within:bg-white/10 focus-within:ring-2 focus-within:ring-red-500/15">
             <Search className="size-5 shrink-0 text-slate-400" />
